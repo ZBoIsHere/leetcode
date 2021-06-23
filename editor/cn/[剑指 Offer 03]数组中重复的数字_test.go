c75818c091b56feeada337106737f027e2c8fdc2
ZBoIsHere <13126896150@163.com>
2021-06-23 22:09:33 +0800

    jianzhioffer 03

---
+++ editor/cn/[剑指 Offer 03]数组中重复的数字_test.go
@@ -0,0 +1,23 @@
+package cn
+
+import "testing"
+
+func Test_findRepeatNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findRepeatNumber(tt.args.nums); got != tt.want {
+				t.Errorf("findRepeatNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
